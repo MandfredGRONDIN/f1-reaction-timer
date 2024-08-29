919becc08b345f28dd826193f104d81f269d375b
Mandfred GRONDIN <mandfred.pro@hotmail.com>
2024-08-29 13:37:30 +0200

    chore: Update ReactionTimesPage to handle empty reaction times list

--- front/src/views/ReactionTimesPage.jsx
+++ front/src/views/ReactionTimesPage.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import api from '../utils/api.jsx'
 import { jwtDecode } from 'jwt-decode'
 import ReactionTimesList from '../components/ReactionTimesList.jsx'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, Link } from 'react-router-dom'
 import '../styles/ReactionTimesPage.css'
 
 function ReactionTimesPage() {
@@ -49,10 +49,22 @@ function ReactionTimesPage() {
     <div className="reaction-times-container">
       <h2>Your Reaction Times</h2>
       {error && <p className="error">{error}</p>}
-      <ReactionTimesList reactionTimes={reactionTimes} />
-      <button onClick={() => navigate(-1)} className="back-button">
-        Back
-      </button>
+      {reactionTimes.length === 0 ? (
+        <div className="no-data">
+          <p>No reaction times found. Why not</p>
+          <Link to="/reaction-timer" className="reaction-timer-link">
+            try the F1 Reaction Timer
+          </Link>
+          ?
+        </div>
+      ) : (
+        <>
+          <ReactionTimesList reactionTimes={reactionTimes} />
+          <button onClick={() => navigate(-1)} className="back-button">
+            Back
+          </button>
+        </>
+      )}
     </div>
   )
 }
